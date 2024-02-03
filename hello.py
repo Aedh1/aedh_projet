fd52e6338dcb56a85a9a42b120ca0f0b670350bf
Aedh1 <86604313+Aedh1@users.noreply.github.com>
2024-02-03 16:51:31 +0100

    afficher les multiples de 1 à 10 du nombre entré

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-a = int(input('donner moi un nombre : '))
+a = int(input('Donnez-moi un nombre : '))
 print(a)
+
+for i in range(1, 11):
+    resultat = a * i
+    print(str(a) + " x " + str(i) + ' = ' + str(resultat))
