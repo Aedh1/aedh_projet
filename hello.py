e360712a4a098302f288c005d7a5c2c68e0dd5a4
Aedh1 <86604313+Aedh1@users.noreply.github.com>
2024-02-03 17:08:46 +0100

    demander à la fin si l'utilisateur souahite terminer le programme ou recommencer

--- hello.py
+++ hello.py
@@ -1,6 +1,17 @@
-a = int(input('Donnez-moi un nombre : '))
-print(a)
+v = True
+while v == True :
+    a = int(input('Donnez-moi un nombre : '))
+    print(a)
 
-for i in range(1, 11):
-    resultat = a * i
-    print(str(a) + " x " + str(i) + ' = ' + str(resultat))
+    for i in range(1, 11):
+        resultat = a * i
+        print(str(a) + " x " + str(i) + ' = ' + str(resultat))
+        
+    k = input("est ce que vous souhaitez terminer le programme (oui/non) : ")
+    if k.lower() == "non":
+        v = False
+    else : 
+        v = "True"
+        
+print("Fin du programme")
+        
